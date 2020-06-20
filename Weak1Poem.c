487db3cf9d41674ac2a513b93da87c3709ba8ad9
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-06-20 13:17:09 +0530

    Add files via upload

---
+++ Weak1Poem.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+printf("It is always the dispossessed— \n someone driving a huge rusted Dodge  \n that’s burning oil, and must cost  \n twenty-five dollars to fill. \n Today before seven I saw, through \n the morning fog, his car leave the road,    \n turning into the field. It must be \n his day off, I thought,...");
+return 0;
+}
